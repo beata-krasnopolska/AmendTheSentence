18bec545d2e177e800ee052db9ae32d13e7bd9ef
Beata-Krasnopolska <beata.krasnopolska@interia.pl>
2018-08-23 18:47:54 +0200

    Add files

---
+++ AmendTheSent/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace AmendTheSent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string s = "CodesignalIsAwesome"; // the output should be amendTheSentence(s) = "codesignal is awesome";
+            string s = "Hello"; //the output should be amendTheSentence(s) = "hello"
+
+            var textTransform = new Transformation();
+            string result = textTransform.AmendTheSentence(s);
+                       
+            //Console.WriteLine("{0}", s);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AmendTheSent/Transformation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace AmendTheSent
+{
+    class Transformation
+    {
+        public string AmendTheSentence(string s)
+        {
+            //StringBuilder builder = new StringBuilder();
+            //foreach (char c in s)
+            //{
+            //    if (Char.IsUpper(c) && builder.Length > 0) builder.Append(' ');
+            //    builder.Append(c);
+            //}
+            //s = builder.ToString();
+            //return s;
+            var r = new Regex(@"
+                (?<=[A-Z])(?=[A-Z][a-z]) |
+                 (?<=[^A-Z])(?=[A-Z]) |
+                 (?<=[A-Za-z])(?=[^A-Za-z])", RegexOptions.IgnorePatternWhitespace);
+                       
+            s = r.Replace(s, " ").ToLower();
+            
+            Console.WriteLine("{0}", s);
+                        
+            return s;
+        }
+    }
+}
